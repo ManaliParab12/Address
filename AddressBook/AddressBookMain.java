8989abd5550a3a8d995251d8d16c34476961c186
ManaliParab12 <manaliparab10@gmail.com>
2020-11-21 08:41:05 +0530

    Using Switch case to Perform Person information Operations like add & edit in main method

--- AddressBook/AddressBookMain.java
+++ AddressBook/AddressBookMain.java
@@ -1,13 +1,38 @@
 package addressBook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+	
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String [] args) {
 		System.out.println("Welcome to Address Book Program");	
 		
 		AddressBook addressbook = new AddressBook();
-		addressbook.addPerson();
-		addressbook.display();
-	}	
+		
+		System.out.println("Select any option to perform Operation of Person ");
+	    System.out.println(" Enter 1 to add Person"
+			           + "\n Enter 2 to Edit Person"
+	    		       + "\n Enter 3 to display Person Information");
+	    
+	    int value = sc.nextInt();
+	    switch (value) {
+		case 1 :
+			addressbook.addPerson();
+			break;
+		case 2 :
+			addressbook.editPerson();
+			break;
+		case 3 :
+			addressbook.display();
+			break;
+		case 4 :				
+			value = 0;
+			break;
+		default :
+			System.out.println("Incorrect Choice" + "\nEnter a number between 1 and 4");			
+	    }
+    }	
 }
 
