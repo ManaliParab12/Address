366b4c55ef63512a976197c71223946652563c71
ManaliParab12 <manaliparab10@gmail.com>
2020-11-22 09:51:09 +0530

    Added deletePerson method in switch case of main class

--- AddressBook/AddressBook.java
+++ AddressBook/AddressBook.java
@@ -96,7 +96,7 @@ public class AddressBook {
 			}
 		}		
 	}
-	
+
 	
 	@Override  
 	protected void finalize() {   
--- AddressBook/AddressBookMain.java
+++ AddressBook/AddressBookMain.java
@@ -11,28 +11,36 @@ public class AddressBookMain {
 		
 		AddressBook addressbook = new AddressBook();
 		
-		System.out.println("Select any option to perform Operation of Person ");
-	    System.out.println(" Enter 1 to add Person"
-			           + "\n Enter 2 to Edit Person"
-	    		       + "\n Enter 3 to display Person Information");
-	    
-	    int value = scanner.nextInt();
-	    switch (value) {
-		case 1 :
-			addressbook.addPerson();
-			break;
-		case 2 :
-			addressbook.editPerson();
-			break;
-		case 3 :
-			addressbook.display();
-			break;
-		case 4 :				
-			value = 0;
-			break;
-		default :
-			System.out.println("Incorrect Choice" + "\nEnter a number between 1 and 4");			
-	    }
-    }	
+		int input=1;
+		while (input == 1) {					
+			System.out.println("Select any option From Menu ");
+			System.out.println("1. Add Person"
+							+ "\n2. Edit Person"
+							+ "\n3. Delete Person"					
+							+ "\n4. Display"
+							+ "\n8. Quit");
+			int value = scanner.nextInt();
+			
+			switch (value) {
+			case 1 :
+				addressbook.addPerson();
+				break;
+			case 2 :
+				addressbook.editPerson();
+				break;
+			case 3 :
+				addressbook.deletePerson();
+				break;			
+			case 4 :
+				addressbook.display();
+				break;
+			case 5 :
+				input = 0;
+				break;
+			default :
+				System.out.println("Incorrect Choice" + "\nEnter a number between 1 and 5");			
+		    }
+		 }
+	}		
 }
 
